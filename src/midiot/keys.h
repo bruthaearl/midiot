8c74361b5e95d80a358a0b93ed9e82e386522299
bruthaearl <jake@oakst.io>
2019-02-02 15:31:07 -0600

    mcu code done

---
+++ src/midiot/keys.h
@@ -0,0 +1,10 @@
+const char* WIFI_SSID = "NETGEAR05";
+const char* WIFI_PASS = "bravesun628";
+
+// Losant credentials.
+const char* LOSANT_DEVICE_ID = "5bc91832c3f6050008c617d1";
+//const char* LOSANT_ACCESS_KEY = "033bade3-f55a-4ac4-808c-8ea7c7f4f0e9";
+//const char* LOSANT_ACCESS_SECRET = "3e6178e4ae4dd028cb2d3f34d75fa6fe5710d013c20d6b9632afd26b7777f597";
+
+const char* LOSANT_ACCESS_KEY = "c38433e0-9eb8-47d3-8492-f4b6698dfc7c";
+const char* LOSANT_ACCESS_SECRET = "4d3c6320fca818d8c34b1b380d8ef6ca485e1ea4b4a4b7aff96a9c16aed3cea8";
--- src/midiot/midiot.ino
+++ src/midiot/midiot.ino
@@ -1,31 +1,92 @@
-/**
- * Workshop example for a simple internet button using the Losant IoT
- * platform.
- *
- * Visit https://www.losant.com/kit for full instructions.
- *
- * Copyright (c) 2016 Losant IoT. All rights reserved.
- * https://www.losant.com
- */
-
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include <MIDI.h>
+#include <WiFi101.h>
 #include <Losant.h>
-
-// WiFi credentials.
-const char* WIFI_SSID = "NETGEAR05";
-const char* WIFI_PASS = "bravesun628";
-
-// Losant credentials.
-const char* LOSANT_DEVICE_ID = "5c323c8b856a620008b8eec7";
-const char* LOSANT_ACCESS_KEY = "033bade3-f55a-4ac4-808c-8ea7c7f4f0e9";
-const char* LOSANT_ACCESS_SECRET = "3e6178e4ae4dd028cb2d3f34d75fa6fe5710d013c20d6b9632afd26b7777f597";
+#include "keys.h"
 
 const int BUTTON_PIN = 5;
+const int LED_PIN = 13;
+const int LED_R_PIN = 10;
+const int LED_G_PIN = 11;
 
-WiFiClientSecure wifiClient;
+int note;
+
+
+bool led_r_state = false;
+
+WiFiSSLClient wifiClient;
 
 LosantDevice device(LOSANT_DEVICE_ID);
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+void setup() {
+  MIDI.begin();     
+  WiFi.setPins(8, 7, 4, 2);
+  Serial.begin(115200);
+  while (!Serial) { }
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_R_PIN, OUTPUT);
+  pinMode(LED_G_PIN, OUTPUT);
+
+  digitalWrite(LED_R_PIN, LOW);
+  digitalWrite(LED_G_PIN, HIGH);
+
+
+
+  // Register the command handler
+  device.onCommand(&handleCommand);
+
+  connect();
+  
+}
+
+void loop() {
+
+  bool toReconnect = false;
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Disconnected from WiFi");
+    toReconnect = true;
+  }
+
+  if (!device.connected()) {
+    Serial.println("Disconnected from Losant");
+    Serial.println(device.mqttClient.state());
+    toReconnect = true;
+  }
+
+  if (toReconnect) {
+    connect();
+  }
+
+  device.loop();
+
+  delay(100);
+}
+
+void play_note(int note){
+  digitalWrite(LED_R_PIN, 0);
+  MIDI.sendNoteOn(note, 127, 1);    // Send a Note (pitch 42, velo 127 on channel 1)
+  delay(100);                // Wait for a second
+  MIDI.sendNoteOff(note, 0, 1);     // Stop the note
+  digitalWrite(LED_R_PIN, 1);  
+}
+
+// Called whenever the device receives a command from the Losant platform.
+void handleCommand(LosantCommand *command) {
+  Serial.print("Command received: ");
+  Serial.println(command->name);
+
+  if (strcmp(command->name, "play") == 0) {
+    JsonObject& payload = *command->payload;
+    payload.printTo(Serial);
+    note = payload["note"];
+    Serial.println(note);
+    play_note(note);
+  }
+  
+}
 
 void connect() {
 
@@ -35,12 +96,7 @@ void connect() {
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
 
-   // WiFi fix: https://github.com/esp8266/Arduino/issues/2186
-  WiFi.persistent(false);
-  WiFi.mode(WIFI_OFF);
-  WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -52,128 +108,19 @@ void connect() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  Serial.print("Authenticating Device...");
-  HTTPClient http;
-  http.begin("http://api.losant.com/auth/device");
-  http.addHeader("Content-Type", "application/json");
-  http.addHeader("Accept", "application/json");
-
-  /* Create JSON payload to sent to Losant
-   *
-   *   {
-   *     "deviceId": "575ecf887ae143cd83dc4aa2",
-   *     "key": "this_would_be_the_key",
-   *     "secret": "this_would_be_the_secret"
-   *   }
-   *
-   */
-
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-  root["deviceId"] = LOSANT_DEVICE_ID;
-  root["key"] = LOSANT_ACCESS_KEY;
-  root["secret"] = LOSANT_ACCESS_SECRET;
-  String buffer;
-  root.printTo(buffer);
-
-  int httpCode = http.POST(buffer);
-
-  if(httpCode > 0) {
-      if(httpCode == HTTP_CODE_OK) {
-          Serial.println("This device is authorized!");
-      } else {
-        Serial.println("Failed to authorize device to Losant.");
-        if(httpCode == 400) {
-          Serial.println("Validation error: The device ID, access key, or access secret is not in the proper format.");
-        } else if(httpCode == 401) {
-          Serial.println("Invalid credentials to Losant: Please double-check the device ID, access key, and access secret.");
-        } else {
-           Serial.println("Unknown response from API");
-        }
-      }
-    } else {
-        Serial.println("Failed to connect to Losant API.");
-
-   }
-
-  http.end();  
-
-
-  // Connect to Losant.
   Serial.println();
   Serial.print("Connecting to Losant...");
 
   device.connectSecure(wifiClient, LOSANT_ACCESS_KEY, LOSANT_ACCESS_SECRET);
 
-  while(!device.connected()) {
+  while (!device.connected()) {
+    led_r_state = !led_r_state;
+    digitalWrite(LED_R_PIN, led_r_state);
     delay(500);
     Serial.print(".");
   }
 
+  digitalWrite(LED_R_PIN, 1);
+  digitalWrite(LED_G_PIN, 0);
   Serial.println("Connected!");
-  Serial.println();
-  Serial.println("This device is now ready for use!");
-}
-
-void setup() {
-  Serial.begin(115200);
-
-  // Giving it a little time because the serial monitor doesn't
-  // immediately attach. Want the workshop that's running to
-  // appear on each upload.
-  delay(2000);
-
-  Serial.println();
-  Serial.println("Running Workshop 1 Firmware.");
-
-  pinMode(BUTTON_PIN, INPUT);
-  connect();
-}
-
-void buttonPressed() {
-  Serial.println("Button Pressed!");
-
-  // Losant uses a JSON protocol. Construct the simple state object.
-  // { "button" : true }
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-  root["button"] = true;
-
-  // Send the state to Losant.
-  device.sendState(root);
-}
-
-int buttonState = 0;
-
-void loop() {
-
-  bool toReconnect = false;
-
-  if(WiFi.status() != WL_CONNECTED) {
-    Serial.println("Disconnected from WiFi");
-    toReconnect = true;
-  }
-
-  if(!device.connected()) {
-    Serial.println("Disconnected from Losant");
-    Serial.println(device.mqttClient.state());
-    toReconnect = true;
-  }
-
-  if(toReconnect) {
-    connect();
-  }
-
-  device.loop();
-
-  int currentRead = digitalRead(BUTTON_PIN);
-
-  if(currentRead != buttonState) {
-    buttonState = currentRead;
-    if(buttonState) {
-      buttonPressed();
-    }
-  }
-
-  delay(100);
 }
